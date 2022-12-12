dd062af4191ddfe9e4712ebafefff86cb6533eee
Matthew McDonald <yerwhile@gmail.com>
2022-12-12 09:45:32 -0600

    making mugs

---
+++ main.js
@@ -0,0 +1,9 @@
+const clay = [ "Clay", "Clay", "Clay", "Clay" ]
+const toFireInKiln = []
+
+for (const word of clay) {
+   const mug = `${word} coffee mug`;
+   toFireInKiln.push(mug);
+}
+
+console.log(toFireInKiln);
